a23ddbf145203fe76a8d9a10ac3bef1ded3482cb
AuroSan <aurora_hoshi@live.com>
2016-11-22 17:24:52 -0600

    Clase Menu Instrucciones
    Aqui se muestran las instrucciones del juego

--- MenuInstrucciones.java
+++ MenuInstrucciones.java
@@ -6,7 +6,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author Aurora "Hoshi" Gonzale
  * @version 1.0.15.11.16 
  */
-class MenuInstrucciones extends World{
+class MenuInstrucciones extends World
+{
     private GreenfootImage fondo;   //Variable de la imagen "instrucciones"
     private World w;                //Variable de World
     
@@ -15,16 +16,19 @@ class MenuInstrucciones extends World{
      * author
      * version 1.0.07.11.16
      */
-    public MenuInstrucciones(){    
+    public MenuInstrucciones()
+    {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(850, 480, 1);
         w = new MenuInstrucciones();
         fondo = new GreenfootImage("Instrucciones.png"); //Guarda imagen en memoria
     }
     
-    public void act(){
+    public void act()
+    {
         setBackground(fondo); //Establece imagen como fondo de pantalla.
-        if(Greenfoot.isKeyDown("enter")){
+        if(Greenfoot.isKeyDown("enter"))
+        {
             w = new MenuPrincipal();
             Greenfoot.setWorld(w);
         }
