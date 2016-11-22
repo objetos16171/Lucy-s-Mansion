8c93d22aea03ed8ae13de0c12be87b08a6772452
damianMartinez73 <damian.e.martinez@gmail.com>
2016-11-22 17:15:06 -0600

    Clase de la utilería

--- Utileria.java
+++ Utileria.java
@@ -1,7 +1,7 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * SuperClase para los muebles, no es gran cosa, pero de aquí habrá distintos muebles, imagenes y posiciones.
+ * SuperClase para los muebles, no es gran cosa, pero de aquí habrá distintos muebles, imágenes y posiciones.
  * 
  * @author Damián "Mad" Martínez
  * @author Aurora "Hoshi" Gonzalez
@@ -9,18 +9,78 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * 
  * @version 1.0:03.11.16
  */
-
 public class Utileria extends Actor
 {
     protected int ubicacionX;
     protected int ubicacionY;
     protected GreenfootImage imagenMueble;
+    private Jugador player;
+   
+    public Utileria(int x, int y, String cadena)
+    { 
+        ubicacionX = x;
+        ubicacionY = y;
+        //imagenMueble = new GreenfootImage(cadena);
+    }
+        
+    public void act()
+    {  
+        if(isTouching(Jugador.class))
+        {
+            Escenario w =(Escenario) getWorld();
+            player = w.getJugador();
+            if(player.getIsAttack())
+                destruye();
+        }
+        else if(isTouching(Disparo.class))
+           destruye();
+    }
+  
+    /**
+     * Drop
+     * se usa un random para ver si tiene un elemento de la subclase PowerUp
+     * según sea el caso aperecera una llave,un muffin o una pastilla
+     * @author Lucy Cárdenas
+     * 
+     * @version 15.11.16 
+     */
+    public void dropObj()
+    {
+        int aleatorio = Greenfoot.getRandomNumber(100);
+        if(aleatorio < 10)
+        {
+             getWorld().addObject(new Pastilla(), ubicacionX, ubicacionY);
+        }
+        else if(aleatorio > 10 && aleatorio < 20)
+        {
+             getWorld().addObject(new Muffin(), ubicacionX, ubicacionY);
+        }
+    }    
     
-    /*public Mueble(int X, int Y, String Cadena){
-        ubicacionX = X;
-        ubicacionY = Y;
-        imagenMueble = new GreenfootImage(Cadena);
-    }*/
+    /**
+     * Explota
+     * sirve para checar que si el objeto recibe 3 golpes se destruya   
+     * se elimina la mesita, la sillita,el silloncito,el maguey.        
+     * @author Lucy Cárdenas
+     * 
+     * @version 16.11.16 
+     */
+    public void explota()
+    {
+        imagenMueble = new GreenfootImage("explosion.png");
+        getWorld().repaint();
+        getWorld().removeObject(this);
+       //Luego se elimina de memoria;
+    }
     
-    
-}
+    /**
+     * destruye - Se encarga de destruir el objeto, en animacion y en memoria
+     * También se encarga de ver si se tira algún objeto
+     */
+    private void destruye()
+    {
+        dropObj();
+        explota();
+    }
+}    
+            
