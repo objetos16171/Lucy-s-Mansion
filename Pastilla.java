88398a7b7531445af6196b7da08e0c383991affb
AuroSan <aurora_hoshi@live.com>
2016-11-23 22:20:35 -0600

    Clase Pastilla
    Clase Pastilla Modificada c:

--- Pastilla.java
+++ Pastilla.java
@@ -1,38 +1,36 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-import java.lang.Class;
 import java.util.List;
 
 /**
  * Write a description of class Pastilla here.
  * 
- * @author (Hoshi) 
- * @version (1.1.14.11.16)
+ * @author Hoshi 
+ * @version 1.1.14.11.16
  */
 public class Pastilla extends PowerUp
 {     
-    private Jugador jug[] = {null}; //Arreglo de imagenes del jugador
+    private Jugador jug; //Apuntador del jugador
     
     /**
      * Constructor que coloca la imagen nwn
-     * @author Aurora 
-     * @version 1.1.14.11.16
+     * @author Aurora
+     * @version 1.2.23.11.16
      */
     public Pastilla()
     {
-        super("pastilla.png");
+        super("pastilla.png"); //Colocar imagen de la pastilla.
     }
     
     /**
      *  Metodo modifica -> modifica los atributos del jugador y la pastilla cada cierto tiempo n.n
      *  @author Aurora
-     *  @version 1.2.15.11.16
+     *  @version 1.5.23.11.16
      */
     public void modifica()
     {
-        World w = getWorld(); //Obtiene el mundo de jugador 
-        jug[0] = w.getObjects(jugador); //Obtener el objeto jugador.
-        jug[0].setAumentoVida(10); //Decirle al jugador que modifique su vida cada cierto tiempo.
-        removeTouching(Pastilla.class); //Quita el objeto "pastilla"
+        Escenario w = (Escenario) getWorld(); //Obtiene el mundo de jugador. 
+        jug = w.getJugador(); //Obtener el objeto jugador. 
+        jug.setAumentoVida(1); //Decirle al jugador que modifique su vida cada cierto tiempo.
+        w.removeObject(this); //Quita el objeto "Pastilla"(Pastilla.class); //Quita el objeto "Pastilla"
     }
 }
-
