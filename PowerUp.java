11cf6f41db6ee94fc1e752e387bfa847ea441573
AuroSan <aurora_hoshi@live.com>
2016-11-24 19:37:53 -0600

    Clase PowerUp
    Clase que apareció salvajemente en mis archivos

--- PowerUp.java
+++ PowerUp.java
@@ -3,11 +3,24 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class PowerUp here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author (Aurora) 
+ * @version (1.1.16.11.16)
  */
-public class PowerUp extends Actor
+public abstract class PowerUp extends Actor
 {
+    private String str;
+    private GreenfootImage imagen;
+    
+    /**
+     * Constructor PowerUp
+     * @author Aurora
+     * @version 1.1.16.11.16
+     */
+    public PowerUp(String str)
+    {
+        imagen = new GreenfootImage(str);
+    }
+    
     /**
      * Act - do whatever the PowerUp wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
@@ -15,5 +28,17 @@ public class PowerUp extends Actor
     public void act() 
     {
         // Add your action code here.
-    }    
+        if(isTouching(Jugador.class)) 
+        {
+            modifica();
+        }
+    }
+    
+    /**
+     * Método protegido y abstracto llamado: modifica()
+     * @author Aurora
+     * @version 1.1.16.11.16
+     * 
+     */
+    protected abstract void modifica();
 }
